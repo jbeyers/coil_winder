311d01ea72a22d9c4a0d06fa58d5e1e70c9051ac
Johan Beyers <johan@juizi.com>
2017-11-13 08:58:41 +0200

    Some tweaks and comments

--- code/coil_winder/coil_winder.ino
+++ code/coil_winder/coil_winder.ino
@@ -4,11 +4,14 @@ const int disable_out = 5;
 const int start_in = 2;
 const int pot_in_an = 14;
 const int pot_deadband = 20;
-const unsigned long steps_to_do = 200 * 10; // steps/rev times revs needed
+const unsigned long turns_to_do = 750ul;
+const unsigned long steps_per_turn = 200ul;
+const unsigned long steps_to_do = turns_to_do * steps_to_do;
 
 unsigned long start_millis;
 int start_latch;
 unsigned long steps_done;
+unsigned long turns_done;
 int pot_center;
 int pot_min;
 int pot_max;
@@ -20,20 +23,29 @@ bool forward;
 bool can_turn;
 
 void setup() { 
+  // Start toggle.
   pinMode(start_in, INPUT_PULLUP);
+  // Speed control potentiometer
   pinMode(pot_in_an, INPUT);
+  // Stepper driver control
   pinMode(step_out, OUTPUT);
   pinMode(dir_out, OUTPUT);
   pinMode(disable_out, OUTPUT);
+  // Initialise values
   pot_max = 0;
   pot_min = 1024;
+  start_millis = millis();
+
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
 } 
 
 void loop() { 
   if (digitalRead(start_in) == LOW) {
     // Active running.
     digitalWrite(disable_out, LOW);
-    // Check if the analog pot is requesting a speed, and scale it
+    // Check if the pot is requesting a speed, and scale it
+    // TODO: Use the Map function to do this.
     pot_value = analogRead(pot_in_an);
     pot_max = max(pot_value, pot_max);
     pot_min = min(pot_value, pot_min);
@@ -51,9 +63,9 @@ void loop() {
           speed = 300*(pot_value - pot_center - pot_deadband)/range;
         }
         speed = speed + 10;
-        step_delay = 800 + (60000/speed);
+        step_delay = 1600 + (60000/speed);
         can_turn = true;
-        if (steps_done == steps_to_do && forward) {
+        if ((steps_done >= steps_to_do) && forward) {
           can_turn = false;
         }
         if (steps_done == 0 && !forward) {
@@ -71,13 +83,24 @@ void loop() {
             }
         }
     }
+    // show the turns completed every second while running.
+    // This could be in an interrupt, but meh
+    if (millis() > start_millis + 1000ul) {
+      turns_done = steps_done/steps_per_turn;
+      Serial.print("turns = ");
+      Serial.print(turns_done);
+      Serial.println();
+      start_millis = start_millis + 1000ul;
+    }
   } else {
     // Reset the values for the counters and centering
     digitalWrite(disable_out, HIGH);
-    steps_done = 0;
+    steps_done = 0ul;
+    // Get the min and max values for the speed control
     pot_value = analogRead(pot_in_an);
     pot_max = max(pot_value, pot_max);
     pot_min = min(pot_value, pot_min);
+    // Set the center point. Note that this will be center when you enable the counter.
     pot_center = pot_value;
   }
 }
